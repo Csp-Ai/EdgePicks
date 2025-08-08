da0425dfdb56ca56ae097b3739573acae0d942f1
Csp-Ai <poncechris95@gmail.com>
2025-08-07 22:41:37 -0700

    chore: update llms log [skip-llms]

--- __tests__/llmsLog.test.ts
+++ __tests__/llmsLog.test.ts
@@ -3,13 +3,21 @@ import path from 'path';
 import crypto from 'crypto';
 import { freezeTime } from './utils/freezeTime';
 
+let unfreeze: () => void;
+
+beforeAll(() => {
+  unfreeze = freezeTime('2025-02-02T02:02:02.000Z');
+});
+
+afterAll(() => {
+  unfreeze();
+});
+
 describe('llms log', () => {
   it('writes entry (stable time)', () => {
-    const unfreeze = freezeTime('2025-02-02T02:02:02.000Z');
     const file = path.join(__dirname, '..', 'llms.txt');
     const content = fs.readFileSync(file, 'utf8');
     const hash = crypto.createHash('sha256').update(content).digest('hex');
-    unfreeze();
     expect(hash).toMatchSnapshot();
   });
 });
