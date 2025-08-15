a25cdfc78201b1492316e5770a709baa0c9681fd
Csp-Ai <poncechris95@gmail.com>
2025-08-14 21:04:10 -0700

    chore: update llms log [skip-llms]

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import UpcomingGamesHero from '@/components/UpcomingGamesHero';
-
-export default function HomePage() {
-  return (
-    <main className="container mx-auto p-6">
-      <UpcomingGamesHero />
-    </main>
-  );
-}
